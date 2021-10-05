93cb8827d540c4af54cb48818522d8fb4f6e9d9f
itsapoorvj <47720632+itsapoorvj@users.noreply.github.com>
2021-10-05 18:23:07 +0530

    added page attribute in functions

--- jobs/jobsearch.py
+++ jobs/jobsearch.py
@@ -1,15 +1,15 @@
 from jobs.get import get_json
 
 def lawjobs(query,page=1):
-	params = {"query": query,"page"=page}
+	params = {"query": query,"page":page}
 	return get_json("/lawjobs/search", params)
 
 def indeed(query,page=1):
-	params = {"query": query,"page"=page}
+	params = {"query": query,"page":page}
 	return get_json("/indeed/search", params)
 
 def simplehired(query,page=1):
-	params = {"query": query,"page"=page}
+	params = {"query": query,"page":page}
 	return get_json("/simplyhired/search", params)
 
 def craigslist(query,region_id):
@@ -20,29 +20,29 @@ def craigslist_regions():
     return get_json("/craigslist/regions")
 
 def careerjet(query,page=1):
-	params = {"query": query,"page"=page}
+	params = {"query": query,"page":page}
 	return get_json("/careerjet/search", params)
 
 def dice(query,page=1):
-	params = {"query": query,"page"=page}
+	params = {"query": query,"page":page}
 	return get_json("/dice/search", params)
 
 def glassdoor(query,page=1):
-	params = {"query": query,"page"=page}
+	params = {"query": query,"page":page}
 	return get_json("/glassdoor/search", params)
 
 def xing(query,page=1):
-	params = {"query": query,"page"=page}
+	params = {"query": query,"page":page}
 	return get_json("/xing/search", params)
 
 def stackoverflow(query,page=1):
-	params = {"query": query,"page"=page}
+	params = {"query": query,"page":page}
 	return get_json("/stackoverflow/search", params)
 
 def monster(query, state="CA"):
-	params = {"query": query,"state"=state}
+	params = {"query": query,"state":state}
 	return get_json("/monster/search", params)
 
 def linkedin(query,page=1):
-	params = {"query": query,"page"=page}
+	params = {"query": query,"page":page}
 	return get_json("/linkedin/search", params)
